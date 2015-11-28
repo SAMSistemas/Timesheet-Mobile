750b012fe577ab063aa47b313cd0585028accb21
jonatan <jonatansalas@live.com.ar>
2015-11-28 00:40:27 -0300

    Added launch effect to the App.

--- view/src/main/java/com/samsistemas/timesheet/activity/LoginActivity.java
+++ view/src/main/java/com/samsistemas/timesheet/activity/LoginActivity.java
@@ -64,6 +64,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_Dark_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
