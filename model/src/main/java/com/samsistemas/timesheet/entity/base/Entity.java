17e373a4636cb5e72787e3c450ffc5f037183d22
jonisaa <jonatansalas@live.com.ar>
2015-12-11 11:13:38 -0300

    Start javadoc documentation for entity classes.

--- model/src/main/java/com/samsistemas/timesheet/entity/base/Entity.java
+++ model/src/main/java/com/samsistemas/timesheet/entity/base/Entity.java
@@ -1,16 +1,33 @@
 package com.samsistemas.timesheet.entity.base;
 
 /**
+ * Base entity class, all entities have to extend this class.
+ *
  * @author jonatan.salas
  */
 public class Entity {
+
+    /**
+     * The id to be saved.
+     */
     private long id;
 
+    /**
+     * Id setter as builder pattern.
+     *
+     * @param id the id associated to this entity
+     * @return the entity object, with id set
+     */
     public Entity setId(long id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * Id getter
+     *
+     * @return the id set for an entity object
+     */
     public long getId() {
         return id;
     }
