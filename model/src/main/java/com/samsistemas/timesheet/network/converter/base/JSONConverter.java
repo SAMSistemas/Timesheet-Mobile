bc9721078a76e1cc592ffa2589d57d677b545006
jonatan <jonatansalas@live.com.ar>
2015-12-16 16:37:26 -0300

    Added doc to JSONConverter

--- model/src/main/java/com/samsistemas/timesheet/network/converter/base/JSONConverter.java
+++ model/src/main/java/com/samsistemas/timesheet/network/converter/base/JSONConverter.java
@@ -11,25 +11,28 @@ import org.json.JSONObject;
 import java.util.List;
 
 /**
+ * Interface used for JSON conversion
  *
  * @author jonatan.salas
- * @param <T>
+ * @param <T> class that extends Entity
  */
 public interface JSONConverter<T extends Entity> {
 
     /**
+     * Method that converts a JSONObject as a T class object.
      *
-     * @param object
-     * @return
-     * @throws JSONException
+     * @param object JSONObject to convert
+     * @return a T object
+     * @throws JSONException exception thrown if JSON is malformed or can't read any field
      */
     T asObject(@NonNull JSONObject object) throws JSONException;
 
     /**
+     * Method that converts a JSONArray as a List of T class object.
      *
-     * @param array
-     * @return
-     * @throws JSONException
+     * @param array JSONArray to convert
+     * @return a List of T object
+     * @throws JSONException exception thrown if JSON is malformed or can't read any field
      */
     List<T> asList(@NonNull JSONArray array) throws JSONException;
 }
